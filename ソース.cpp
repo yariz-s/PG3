8b02f3d20345db6d7335050f1cdb800be1a27cde
k024g1027@g.neec.ac.jp <k024g1027@g.neec.ac.jp>
2025-05-16 15:25:15 +0900

    p

---
+++ ソース.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+
+// 関数ポインタPFuncを定義
+typedef void (*PFunc)(int*, int*);
+
+// コールバック関数
+void DispResult(int* s, int* kazu) {
+    int kekka = rand() % 2;  // ランダムで0か1を決定
+
+    if (kekka == *kazu) {
+        if (kekka == 0)
+            printf("%dで丁（偶数）でした! 大当たり!!\n", kekka);
+        else
+            printf("%dで半（奇数）でした! 大当たり!!\n", kekka);
+    }
+    else {
+        if (kekka == 0)
+            printf("%dで丁（偶数）でした! 残念!!\n", kekka);
+        else
+            printf("%dで半（奇数）でした! 残念!!\n", kekka);
+    }
+}
+
+// コールバック関数を呼び出す
+void setTimeout(PFunc p, int second, int kazu) {
+    puts("さて結果は…\n");
+
+    for (int i = 0; i < second; i++) {
+        Sleep(1000);  // 1000ミリ秒＝1秒
+        printf("%d...\n", second - i);
+    }
+
+    p(&second, &kazu);
+}
+
+int main() {
+    int kazu;
+
+    srand((unsigned int)time(NULL));  // 乱数の種を設定
+
+    printf("丁（偶数）ならゼロ、半（奇数）なら1を入力してください\n");
+    scanf_s("%d", &kazu);
+
+    if (kazu == 0) {
+        puts("あなたは丁（偶数）を選びましたね？");
+    }
+    else {
+        puts("あなたは半（奇数）を選びましたね？");
+    }
+
+    PFunc p;
+    p = DispResult;  // DispResult関数のアドレスを代入
+    setTimeout(p, 3, kazu);
+
+    return 0;
+}
